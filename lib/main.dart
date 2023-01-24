ed48539121e1d5580dc6f79b4763385914aaf325
Kevyn <kevyn.santos@mindsatwork.com.br>
2023-01-24 01:52:30 -0300

    Organizando o FutureBuilder para tela de login ou home

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,33 @@
+import 'package:barber_shop_freestyle/pages/Login.dart';
+import 'package:barber_shop_freestyle/services/loginService.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+bool existsLogin = false;
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  Future<String> checkLogin(BuildContext context)
+  async {
+    existsLogin = await LoginService.checkLogin();
+    return [''].join('');
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const MyHomePage(),
+    return FutureBuilder<String>(
+        future: checkLogin(context), // a previously-obtained Future<String> or null
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          return MaterialApp(
+              home: existsLogin?MyHomePage():Login()
+          );
+        }
+
     );
   }
 }
